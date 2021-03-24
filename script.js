4c0b87c0324a74ad731ed3e82fe56f60038678d8
Nick Link <nlink2021@gmail.com>
2021-03-23 20:49:29 -0500

    added pictures and main functionality

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 console.log("linked!")
 
 let mainContainer
+
+
 const mainMenu = document.querySelector('.mainMenu')
 const mainButton = document.querySelector('#mainButton')
 const cellButton = document.querySelectorAll('.box')
+
+const columns = 6
+const rows = 6
 
 mainButton.addEventListener('click', () => {
     toGameBoard()
@@ -15,36 +20,118 @@ mainButton.addEventListener('click', () => {
 //     })
 // }
 
-const setUp = () => {
+// const setUp = () => {
 
-mainContainer = [
+// mainContainer = [
 
-'', '', '', '', '', '',
-'', '', '', '', '', '',
-'', '', '', '', '', '',
-'', '2', '6', '', '', '',
-'', '', '', '', '', '2',
-'', '', '', '', '4', '',
+// ['', '', '', '', '2', '4']
+// ['', '', '', '2', '4', '']
+// ['', '', '2', '4', '', '']
+// ['', '2', '4', '', '', '']
+// ['2', '4', '', '', '', '']
+// ['4', '', '', '', '', '']
 
-]
-}
+// ]
+
+// }
 
 
 const sayHello = () => {
     console.log("clicked")
 }
 
-cellButton.forEach(cell => cell.addEventListener('click', () => {
-    sayHello()
-    if (cell.innerText < 4) {
-        cell.innerText++
-    } else {
-        cell.innerText = 1
+const getBoardValues = () => {
+
+    let cellValues = []
+
+    for (row = 1; row <= columns; row++) {
+        for (col = 1; col <= rows; col++) {
+            console.log(`row: ${row} column: ${col}`)
+            cellValues.push(document.getElementById(`r${row}c${col}`).className.slice(-1));
+
+        }
     }
-    
-    // cell.innerText = 4
-})
-)
+
+    console.log(cellValues)
+    return cellValues;
+}
+
+cellButton.forEach(cell => cell.addEventListener('click', (event) => {
+
+    sayHello()
+
+    // for(let i = 0; i < 4; i++) {
+        if (cell.classList.contains('pipe1-rotation1')) {
+            event.target.className = ""
+            event.target.classList.add('box')
+            event.target.classList.add('pipe1-rotation2')
+            // cell.classList = "box pipe1-rotation" + i++
+            cell.innerText = 2
+            console.log("if ran")
+            // event.target.classList.add('pipe1-rotation')
+            // cell.innerText++
+        } else if (cell.classList.contains('pipe1-rotation2')) {
+            event.target.className = ""
+            event.target.classList.add('box')
+            event.target.classList.add('pipe1-rotation3')
+
+            cell.innerText = 3
+            console.log("if ran")
+        } else if (cell.classList.contains('pipe1-rotation3')) {
+            event.target.className = ""
+            event.target.classList.add('box')
+            event.target.classList.add('pipe1-rotation4')
+
+            cell.innerText = 4
+            console.log("if ran")
+        } else if (cell.classList.contains('pipe1-rotation4')) {
+            event.target.className = ""
+            event.target.classList.add('box')
+            event.target.classList.add('pipe1-rotation1')
+
+            cell.innerText = 1
+            console.log("if ran")
+        } else if (cell.classList.contains('pipe1-rotation5')) {
+            event.target.className = ""
+            event.target.classList.add('box')
+            event.target.classList.add('pipe1-rotation6')
+
+            cell.innerText = 6
+            console.log("if ran")
+        } else if (cell.classList.contains('pipe1-rotation6')) {
+            event.target.className = ""
+            event.target.classList.add('box')
+            event.target.classList.add('pipe1-rotation5')
+
+            cell.innerText = 5
+            console.log("if ran")
+        } 
+
+        // call
+        const currentBoardValues = getBoardValues();
+    }))
+        
+        // cell.classList.contains('pipe1-rotation2')
+        //  || cell.classList.contains('pipe1-rotation3') || cell.classList.contains('pipe1-rotation4')) {
+        //     event.target.className = ""
+        //     event.target.classList.add('box')
+        //     event.target.classList.add('pipe1-rotation' + i)
+        //     cell.classList = "box pipe1-rotation" + i++
+        //     cell.innerText++ 
+        //     console.log("if ran")
+
+        //     event.target.classList.add('pipe1-rotation')
+        //     cell.innerText++
+        // } else {
+        //     event.target.className = ""
+        //     event.target.classList.add('pipe1-rotation1')
+        //     cell.innerText = 1
+        //     console.log("else ran")
+        // }
+
+    // }
+
+
 
 const toGameBoard = () => {
     mainMenu.classList.add('void')
